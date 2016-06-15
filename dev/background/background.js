f70fc1c444bc9ae8c707cfee00049957f5444ea2
ChucKN0risK <lchenais@gmail.com>
2016-06-15 23:01:28 +0200

    Update Buttons Components

--- dev/background/background.js
+++ dev/background/background.js
@@ -1,4 +1,4 @@
-// When the extension is installed then the user is redirected on flppn.com
+//When the extension is installed then the user is redirected on flppn.com
 // chrome.runtime.onInstalled.addListener(function () {
-//   chrome.tabs.create({"url": "http://flppn.com"})
+//   chrome.tabs.create({"url": "http://flppn.com/register"})
 // });
